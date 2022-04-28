f9b5396688fed01af97febc8880c15612d812491
Enshinyan <emeka.enshinyan@gmail.com>
2022-04-27 20:29:27 -0700

    edit: trying gridbaglayout

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,33 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Test extends JPanel {
 
     public static void main(String[] args) {
 
+
+
         JFrame frame = new JFrame("top level container");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        GridLayout grid = new GridLayout(25,25,0,0);
-        JPanel contentPane = new JPanel(grid);
-        contentPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        frame.setSize(700, 800);
 
-        System.out.println(contentPane.getMaximumSize());
-        System.out.println(contentPane.getPreferredSize());
-        System.out.println(grid.getColumns());
-        System.out.println(grid.getRows());
-        System.out.println(grid);
-
-        contentPane.setBackground(Color.BLACK);
-        for (int i = 0; i < grid.getRows() * grid.getColumns() - 20; i++){
-
-            contentPane.add("boxes", new Boxes());
-        }
-
-
-        frame.add(contentPane);
-
-
+        GridBagLayout grid = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridheight = 100;
+        constraints.gridwidth = 100;
+        grid.setConstraints(new JButton("hello"), constraints);
+        frame.setLayout(grid);
         frame.setVisible(true);
     }
 
@@ -35,16 +27,12 @@ public class Test extends JPanel {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             // Draw Text
-            g.setColor(Color.black);
-            g.drawRect(0,0,200,200);
-        }
-    }
-    static class Cell extends JPanel{
-        public void paintComponents(Graphics g){
-            super.paintComponents(g);
             g.setColor(Color.BLACK);
-            g.fillRect(0,0,200,200);
-
+            g.drawRect(1,1,10,10);
         }
+
     }
-}
+
+
+
+    }
